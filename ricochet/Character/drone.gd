3c72366eda9b50c30f936884c0bc5c00aa277ca1
Joe <107723566+VincentVanJoey@users.noreply.github.com>
2025-02-02 02:07:39 -0600

    Drone work to sync with hunter bullet

--- ricochet/Character/drone.gd
+++ ricochet/Character/drone.gd
@@ -1,66 +1,56 @@
 extends Node2D
 
 # Declare the projectile scene
-@export var projectile_scene: PackedScene
-@export var fire_rate: float = 1.0  # Default fire rate (in seconds)
+@export var bullet_scene: PackedScene
+@export var timer_label : Label
+@export var level_timer : Timer
 @export var projectile_speed: float = 500.0  # Speed of the projectiles
 
-var fire_timer: Timer
-var level_type: int = 0  # We will assign this based on the parent level
+const SPEED = 500.0
+
 var parent = get_parent()
 
 func _ready():
-	# Create and set up the timer to control firing rate
-	fire_timer.wait_time = fire_rate
-	fire_timer.start()
+	configure_level_type()
 
+func configure_level_type():
+	
+	#TODO - set timer for level
+	#TODO - figure out bullet pattern
+	#TODO - fire set amounts of bullets depending
+	#TODO - perform checks for time thresholds to change or shoot more
+	
 	# Detect the parent node (level)
 	if parent:
 		match parent.name:  # Assuming parent node names represent different levels
 			"Level1":
-				level_type = 1
+				level_timer.wait_time = 60000
+				level_timer.start()
+				fire_projectile(Vector2.DOWN)
+				pass
 			"Level2":
-				level_type = 2
+				pass
 			"Level3":
-				level_type = 3
+				pass
 			_: #default case
-				level_type = 0
-				
-	# Configure the behavior based on the level type
-	configure_fire_behavior()
-
-# Function to configure fire behavior based on the level
-func configure_fire_behavior():
-	match level_type:
-		1:  # Level 1 - slower fire rate, fewer projectiles
-			fire_rate = 2.0
-			projectile_speed = 300
-		2:  # Level 2 - moderate fire rate and projectile speed
-			fire_rate = 1.5
-			projectile_speed = 400
-		3:  # Level 3 - faster fire rate, more projectiles
-			fire_rate = 1.0
-			projectile_speed = 500
-		_:  # Default case if no level matches
-			fire_rate = 1.0
-			projectile_speed = 400
-	fire_timer.wait_time = fire_rate  # Update the timer
-
-# This function is called when the fire timer times out
+				pass
+	else:
+		print("We fucked up somewhere")
 
 # Function to instantiate and fire a projectile
-func fire_projectile():
-	if projectile_scene:
-		var projectile = projectile_scene.instantiate()
+func fire_projectile(direction: Vector2):
+	if bullet_scene:
+		var projectile := bullet_scene.instantiate()
 		projectile.position = position  # Start at the turret's position
-		var direction = Vector2(1, 0).rotated(rotation)  # Fire direction (can be modified)
-		projectile.linear_velocity = direction * projectile_speed  # Set the speed of the projectile
+		#TODO - set fire direction and or speed
+		#shit
 		get_parent().add_child(projectile)  # Add projectile to the level scene
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	timer_label.text = str(level_timer.time_left)
+	
+	if level_timer.time_left == 0:
+		print("over")
+	
 	pass
-
-
-func _on_fire_timer_timeout() -> void:
-	pass # Replace with function body.
--- ricochet/Menu Scenes/level_select.gd
+++ ricochet/Menu Scenes/level_select.gd
@@ -12,7 +12,7 @@ func _on_debug_button_pressed() -> void:
 
 func _on_first_level_button_pressed() -> void:
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_file("res://Levels/triangle.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level1.tscn")
 	pass # Replace with function body.
 
 func _on_second_level_button_pressed():
